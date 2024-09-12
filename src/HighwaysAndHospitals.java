95d0b65c600ed76b979be647bfaee60df164e571
Benjamin Chock <benjamin.chock@menloschool.org>
2024-09-11 23:13:00 -0700

    commit 4

--- src/HighwaysAndHospitals.java
+++ src/HighwaysAndHospitals.java
@@ -1,20 +1,88 @@
+import java.util.ArrayList;
+
 /**
  * Highways & Hospitals
  * A puzzle created by Zach Blick
  * for Adventures in Algorithms
  * at Menlo School in Atherton, CA
  *
- * Completed by: [YOUR NAME HERE]
+ * Completed by: Benjamin Chock
  *
  */
 
-public class HighwaysAndHospitals {
 
+
+public class HighwaysAndHospitals {
+private static int n;
+private static int hospitalCost;
+private static int highwayCost;
+private static int[][] cities;
+private static ArrayList <ArrayList> citiesTouching;
     /**
      * TODO: Complete this function, cost(), to return the minimum cost to provide
      *  hospital access for all citizens in Menlo County.
      */
-    public static long cost(int n, int hospitalCost, int highwayCost, int cities[][]) {
+    public static long cost(int n1, int hospitalCost1, int highwayCost1, int cities1[][]) {
+
+        n = n1;
+        hospitalCost = hospitalCost1;
+        highwayCost = highwayCost1;
+        cities = cities1;
+        citiesTouching = new ArrayList<ArrayList>();
+        citiesTouching.add(findTouching(1));
+        int current = 0;
+        while (getSizeofArrays() < n){
+            citiesTouching.add(findTouching(findmissingCities(citiesTouching.get(current))));
+            current++;
+        }
+
+
+
+
+        for (int i = 0; i < citiesTouching.get(0).size(); i++){
+            System.out.print(citiesTouching.get(0).get(i));
+        }
+        System.out.println();
+        for (int i = 0; i < citiesTouching.get(1).size(); i++){
+            System.out.print(citiesTouching.get(1).get(i));
+        }
+
+//
         return 0;
     }
+
+    public static ArrayList<Integer> findTouching(int toFind){
+        ArrayList<Integer> holding = new ArrayList<Integer>();
+        holding.add(toFind);
+        int newToFind;
+        int counter = 0;
+        while (counter < holding.size()){
+            newToFind = holding.get(counter);
+            for (int i = 0; i < cities.length; i++){
+                if (cities[i][0] == newToFind && !holding.contains(cities[i][1])){
+                    holding.add(cities[i][1]);
+                }
+            }
+            counter++;
+        }
+        return holding;
+    }
+
+    public static int getSizeofArrays(){
+        int counter = 0;
+        for (int i = 0; i < citiesTouching.size(); i++){
+            counter += citiesTouching.get(i).size();
+        }
+        return counter;
+    }
+
+    public static int findmissingCities(ArrayList<Integer> cities){
+        for (int i = 0; i < cities.size(); i++){
+            if (cities.get(i) != i+1){
+                return i+1;
+            }
+        }
+        return 0;
+    }
+
 }
